de91a09a93bccd8317e0e84e70e8b0587355fe3d
DaniyarM. <gitmailhub@gmail.com>
2024-10-10 20:23:52 +0600

    Initial commit

---
+++ src/main/java/kg/attractor/financial_statement/FinancialStatementApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.financial_statement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialStatementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialStatementApplication.class, args);
+    }
+
+}

5cc1be28cf8325dcd805734ceb26e6025e71040d
abdyrakhman <medzholdoshbaev@gmail.com>
2024-10-13 22:52:10 +0600

    FS-11: added method find by login

--- src/main/java/kg/attractor/financial_statement/repository/UserRepository.java
+++ src/main/java/kg/attractor/financial_statement/repository/UserRepository.java
@@ -4,4 +4,5 @@ import kg.attractor.financial_statement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByLogin(String login);
 }

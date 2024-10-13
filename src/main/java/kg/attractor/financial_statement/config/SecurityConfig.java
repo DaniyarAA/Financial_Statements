6ebe8cac00acbc4371367fc3bf24dc8e0986d70c
abdyrakhman <medzholdoshbaev@gmail.com>
2024-10-13 22:51:52 +0600

    FS-12: added clear sequrity for test

---
+++ src/main/java/kg/attractor/financial_statement/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package kg.attractor.financial_statement.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().permitAll());
+        return http.build();
+    }
+
+
+
+
+}

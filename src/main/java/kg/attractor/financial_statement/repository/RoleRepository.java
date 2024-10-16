2e920c979bd7b119f6caaece1f1d99d8ac033f9d
abdyrakhman <medzholdoshbaev@gmail.com>
2024-10-16 17:17:12 +0600

    FS-11: clear un use methods

--- src/main/java/kg/attractor/financial_statement/repository/RoleRepository.java
+++ src/main/java/kg/attractor/financial_statement/repository/RoleRepository.java
@@ -4,5 +4,4 @@ import kg.attractor.financial_statement.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByRoleName(String roleName);
 }

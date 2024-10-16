141de13bcac0a5a68a1ab8215b8dc4af97222fc7
DaniyarM. <gitmailhub@gmail.com>
2024-10-16 18:39:36 +0600

    Renamed endpoint names

--- src/main/java/kg/attractor/financial_statement/dto/CompanyDto.java
+++ src/main/java/kg/attractor/financial_statement/dto/CompanyDto.java
@@ -12,37 +12,37 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CompanyDto {
     private Long id;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String name;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String inn;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String directorInn;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String login;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String password;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String ecp;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String kabinetSalyk;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String kabinetSalykPassword;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String taxMode;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String opf;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String districtGns;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String socfundNumber;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String registrationNumberMj;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String okpo;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String director;
-    @NotBlank(message = "Обязательно для запонения!")
+    @NotBlank(message = "Обязательно для заполнения!")
     private String ked;
     private String email;
     private String emailPassword;

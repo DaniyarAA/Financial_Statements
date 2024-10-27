f3d37a82c0965f7e95468cb59ef31102724d982b
Kamilla Osmonova <kameakadze@gmail.com>
2024-10-27 18:06:51 +0600

    FS-18 created endpoint for delete user

--- src/main/java/kg/attractor/financial_statement/controller/AdminController.java
+++ src/main/java/kg/attractor/financial_statement/controller/AdminController.java
@@ -1,5 +1,6 @@
 package kg.attractor.financial_statement.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import kg.attractor.financial_statement.dto.CreateRoleDto;
 import kg.attractor.financial_statement.dto.EditUserDto;
@@ -11,13 +12,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("admin")
@@ -43,7 +49,7 @@ public class AdminController {
         }
         try {
             userService.registerUser(userDto);
-            return "redirect:/";
+            return "redirect:/admin/users";
         } catch (IllegalArgumentException e) {
             model.addAttribute("roles", roleService.getAll());
             model.addAttribute("error", e.getMessage());
@@ -74,6 +80,7 @@ public class AdminController {
             model.addAttribute("roles", roleService.getAll());
             return "admin/edit_user";
         }
+
         try {
             userService.updateUser(id, userDto);
             return "redirect:/admin/users";
@@ -93,12 +100,21 @@ public class AdminController {
     }
 
     @PostMapping("create/role")
-    public String createRole(@Valid CreateRoleDto roleDto, BindingResult bindingResult, Model model) {
+    public String createRole(@Valid CreateRoleDto roleDto, BindingResult bindingResult, Model model,
+                             HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             model.addAttribute("createRoleDto", roleDto);
             return "admin/create_role";
         }
         roleService.createNewRole(roleDto);
-        return "redirect:/admin/users";
+        String referer = request.getHeader("Referer");
+        return "redirect:" + (referer != null ? referer : "/admin/users");
     }
+
+    @DeleteMapping("/user/delete/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
+    }
+
 }

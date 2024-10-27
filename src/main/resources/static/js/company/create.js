cb210a62e6873ab8cd2e332f743816fec559c426
abdyrakhman <medzholdoshbaev@gmail.com>
2024-10-27 18:57:58 +0600

    FS-17: clear compromats

--- src/main/resources/static/js/company/create.js
+++ src/main/resources/static/js/company/create.js
@@ -2,13 +2,11 @@ function toggleCreateForm() {
     const createCompanyForm = document.getElementById('createCompanyForm');
     const companyInfo = document.getElementById('companyInfo');
 
-    // If the createCompanyForm is currently displayed, hide it and show companyInfo
     if (createCompanyForm.style.display === "block") {
-        createCompanyForm.style.display = "none"; // Hide the form
-        companyInfo.style.display = "block"; // Show companyInfo
+        createCompanyForm.style.display = "none";
+        companyInfo.style.display = "block";
     } else {
-        // Otherwise, show createCompanyForm and hide companyInfo
-        companyInfo.style.display = "none"; // Hide companyInfo
-        createCompanyForm.style.display = "block"; // Show the form
+        companyInfo.style.display = "none";
+        createCompanyForm.style.display = "block";
     }
 }

87ff3f54c2cfe225f868ed794c020173982c4b33
Kamilla Osmonova <kameakadze@gmail.com>
2024-10-27 18:07:25 +0600

    FS-18 added field RoleDto for changing roles

--- src/main/java/kg/attractor/financial_statement/dto/EditUserDto.java
+++ src/main/java/kg/attractor/financial_statement/dto/EditUserDto.java
@@ -1,5 +1,6 @@
 package kg.attractor.financial_statement.dto;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -32,4 +33,7 @@ public class EditUserDto {
     @NotNull(message = "Заполните дату рождения сотрудника!")
     private LocalDate birthday;
     private MultipartFile avatar;
+
+    @Valid
+    private RoleDto roleDto;
 }

c82d23dde2dec46ccfc979731bf9b0d11db4605d
Kamilla Osmonova <kameakadze@gmail.com>
2024-11-03 14:57:30 +0600

    FS-24 created AuthorityDto class for change role's authorities

---
+++ src/main/java/kg/attractor/financial_statement/dto/AuthorityDto.java
@@ -0,0 +1,15 @@
+package kg.attractor.financial_statement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorityDto {
+    private Long id;
+    private String authority;
+    private String authorityName;
+    private boolean selected;
+}

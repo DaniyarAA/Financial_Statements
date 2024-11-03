5cd0b2a78df177d24e0e0385b9005e5ba94c8eb6
Kamilla Osmonova <kameakadze@gmail.com>
2024-11-03 20:48:59 +0600

    FS-24 added builder for AuthorityDto

--- src/main/java/kg/attractor/financial_statement/dto/AuthorityDto.java
+++ src/main/java/kg/attractor/financial_statement/dto/AuthorityDto.java
@@ -1,12 +1,14 @@
 package kg.attractor.financial_statement.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AuthorityDto {
     private Long id;
     private String authority;

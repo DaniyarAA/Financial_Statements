f5aefbd14bdfa6b0f0c02b999105841026a0d2f7
abdyrakhman <medzholdoshbaev@gmail.com>
2024-11-06 14:55:01 +0600

    FS-32: created request for check unique company name

--- src/main/java/kg/attractor/financial_statement/repository/CompanyRepository.java
+++ src/main/java/kg/attractor/financial_statement/repository/CompanyRepository.java
@@ -23,4 +23,7 @@ public interface CompanyRepository extends JpaRepository<Company, Long> {
 
     @Query("select c from Company c where c.isDeleted = :isDeleted")
     List<Company> findByIsDeleted(@Param("isDeleted") Boolean isDeleted);
+
+
+    Boolean existsByName(String name);
 }

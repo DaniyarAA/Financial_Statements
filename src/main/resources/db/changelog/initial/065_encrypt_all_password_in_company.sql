fbf0dc4f0a98cab9fa0f374929e2f81c3e00946a
abdyrakhman <medzholdoshbaev@gmail.com>
2024-12-08 17:27:53 +0600

    FS-79: corrected database after added decoder and encoder passwords

---
+++ src/main/resources/db/changelog/initial/065_encrypt_all_password_in_company.sql
@@ -0,0 +1,8 @@
+UPDATE companies
+SET password = 'MTIz',
+    kabinet_salyk_password = 'c2FseWtfcGFzc3dvcmQ=',
+    email_password = 'ZW1haWxfcGFzc3dvcmQ=',
+    esf_password = 'ZXNmX3Bhc3N3b3Jk',
+    kkm_password = 'a2ttX3Bhc3N3b3Jk',
+    fresh_1c_password = 'MWNfcGFzc3dvcmQ=',
+    ettn_password = 'ZXR0bl9wYXNzd29yZA==';

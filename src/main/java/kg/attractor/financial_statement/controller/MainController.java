8b7da0c3663627f502f15917e7b33ff896038a01
abdyrakhman <medzholdoshbaev@gmail.com>
2024-12-26 08:24:19 +0600

    FS-111: added logic to check user authority for document tools and status tools open

--- src/main/java/kg/attractor/financial_statement/controller/MainController.java
+++ src/main/java/kg/attractor/financial_statement/controller/MainController.java
@@ -35,7 +35,7 @@ public class MainController {
     @GetMapping
     public String getMainPage(@RequestParam(required = false, defaultValue = "desc") String sort,
                               @RequestParam(required = false, defaultValue = "endDate") String sortBy,
-                              Model model, HttpServletRequest request) {
+                              Model model, HttpServletRequest request,Principal principal) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String login = auth.getName();
         User userDto = userService.getUserByLogin(login);
@@ -50,7 +50,15 @@ public class MainController {
 
         CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
         model.addAttribute("csrfToken", csrfToken);
+        boolean canOpenDocumentTools = false;
+        boolean canOpenTaskTools = false;
+        if (principal != null) {
+            canOpenDocumentTools = userService.canOpenDocumentTools(principal.getName());
+            canOpenTaskTools = userService.canOpenTaskTools(principal.getName());
+        }
 
+        model.addAttribute("canOpenDocumentTools", canOpenDocumentTools);
+        model.addAttribute("canOpenTaskTools", canOpenTaskTools);
         model.addAttribute("companies", companyService.getAllCompanies());
         model.addAttribute("userDto", userDto);
         model.addAttribute("dateUtils", new DateUtils());

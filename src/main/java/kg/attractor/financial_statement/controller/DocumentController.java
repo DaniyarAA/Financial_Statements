3f29ebc4b58d460d3cdbc90daabb3517c4a1b14f
abdyrakhman <medzholdoshbaev@gmail.com>
2024-12-28 10:46:40 +0600

    FS-111:created edit controllers for document type and status

--- src/main/java/kg/attractor/financial_statement/controller/DocumentController.java
+++ src/main/java/kg/attractor/financial_statement/controller/DocumentController.java
@@ -2,12 +2,22 @@ package kg.attractor.financial_statement.controller;
 
 import kg.attractor.financial_statement.service.DocumentTypeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/document")
 public class DocumentController {
     private final DocumentTypeService documentTypeService;
+
+    @PostMapping("/edit")
+    @ResponseBody
+    public ResponseEntity<Map<String, String>> editDocument(@RequestBody Map<String, String> data, Principal principal) {
+        return documentTypeService.edit(data, principal.getName());
+    }
 }
--- src/main/java/kg/attractor/financial_statement/controller/StatusController.java
+++ src/main/java/kg/attractor/financial_statement/controller/StatusController.java
@@ -2,12 +2,25 @@ package kg.attractor.financial_statement.controller;
 
 import kg.attractor.financial_statement.service.TaskStatusService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/status")
 public class StatusController {
     private final TaskStatusService taskStatusService;
+
+    @PostMapping("/edit")
+    @ResponseBody
+    public ResponseEntity<Map<String, String>> editStatus(@RequestBody Map<String, String> data, Principal principal) {
+        return taskStatusService.edit(data,principal.getName());
+    }
 }
